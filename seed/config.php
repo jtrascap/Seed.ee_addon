b8b69d1fc8575a3a040d1806dfced1f1a3ec1d2b
Joel Bradbury <joelbradbury@gmail.com>
2012-06-25 20:55:12 +0100

    Bumped version number to 0.8 for private beta

--- seed/config.php
+++ seed/config.php
@@ -4,7 +4,7 @@
  * Seed Config File
  *
  * @package         seed_ee_addon
- * @version         0.7.4
+ * @version         0.8
  * @author          Joel Bradbury ~ <joel@squarebit.co.uk>
  * @link            http://squarebit.co.uk/seed
  * @copyright       Copyright (c) 2012, Joel 
@@ -14,7 +14,7 @@ if ( ! defined('SEED_NAME'))
 {
 	define('SEED_NAME',         'Seed');
 	define('SEED_CLASS_NAME',   'Seed');
-	define('SEED_VERSION',      '0.7.4');
+	define('SEED_VERSION',      '0.8');
 	define('SEED_DOCS',         'http://squarebit.co.uk/seed');
 	define('SEED_DEBUG',        TRUE);
 
